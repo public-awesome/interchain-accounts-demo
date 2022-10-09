b5d5c95812889d0904d315a52ae60562ef547957
jhernandezb <contact@jhernandez.me>
2022-10-09 17:59:29 -0600

    add config command

--- cmd/icad/cmd/root.go
+++ cmd/icad/cmd/root.go
@@ -164,6 +164,7 @@ func initRootCmd(rootCmd *cobra.Command, encodingConfig params.EncodingConfig) {
 		AddGenesisAccountCmd(app.DefaultNodeHome),
 		tmcli.NewCompletionCmd(rootCmd, true),
 		debug.Cmd(),
+		config.Cmd(),
 		// this line is used by starport scaffolding # stargate/root/commands
 	)
 
